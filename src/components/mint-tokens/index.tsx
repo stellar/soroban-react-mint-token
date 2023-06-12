9227f620abd263c7c77d4a4ec82900916d1cd631
aristides <aristides@aristidesstaffieri.com>
2023-06-12 11:25:30 -0600

    adds basic repo structure, configs, docs, and step 1 (#1)
    * adds basic repo structure, configs, docs, and step 1
    
    * updates bool name to match product conventions

---
+++ src/components/mint-tokens/index.tsx
@@ -0,0 +1,111 @@
+import React from "react";
+import { createPortal } from "react-dom";
+import {
+  Card,
+  Caption,
+  Layout,
+  Notification,
+  Profile,
+} from "@stellar/design-system";
+import freighterApi from "@stellar/freighter-api";
+
+import {
+  connectNetwork,
+  Networks,
+  NetworkDetails,
+} from "../../helpers/network";
+import { ERRORS } from "../../helpers/error";
+import { ConnectWallet } from "./connect-wallet";
+
+import "./index.scss";
+
+type StepCount = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+
+interface MintTokenProps {
+  isShowingHeader?: boolean;
+}
+
+export const MintToken = (props: MintTokenProps) => {
+  const isShowingHeader = props.isShowingHeader === undefined ? true : props.isShowingHeader;
+  const [activeNetworkDetails, setActiveNetworkDetails] = React.useState(
+    {} as NetworkDetails,
+  );
+  const [activePubKey, setActivePubKey] = React.useState(null as string | null);
+  const [stepCount, setStepCount] = React.useState(1 as StepCount);
+  const [connectionError, setConnectionError] = React.useState(
+    null as string | null,
+  );
+
+  function renderStep(step: StepCount) {
+    switch (step) {
+      case 1:
+      default: {
+        const onClick =
+          activeNetworkDetails.network && connectionError === null
+            ? () => setStepCount((stepCount + 1) as StepCount)
+            : setConnection;
+        return (
+          <ConnectWallet
+            network={activeNetworkDetails.network}
+            connectionError={connectionError}
+            onClick={onClick}
+          />
+        );
+      }
+    }
+  }
+
+  async function setConnection() {
+    setConnectionError(null);
+    setActivePubKey(null);
+
+    const isConnected = await freighterApi.isConnected();
+
+    if (!isConnected) {
+      setConnectionError(ERRORS.FREIGHTER_NOT_AVAILABLE);
+      return;
+    }
+
+    const { networkDetails, pubKey } = await connectNetwork();
+
+    if (networkDetails.network !== Networks.Futurenet) {
+      setConnectionError(ERRORS.UNSUPPORTED_NETWORK);
+    }
+
+    setActiveNetworkDetails(networkDetails);
+    setActivePubKey(pubKey);
+  }
+
+  return (
+    <>
+      {isShowingHeader && (
+        <Layout.Header hasThemeSwitch projectId="soroban-react-mint-token" />
+      )}
+      <div className="Layout__inset account-badge-row">
+        {activePubKey !== null && (
+          <Profile isShort publicAddress={activePubKey} size="sm" />
+        )}
+      </div>
+      <div className="Layout__inset layout">
+        <div className="admin-banner-container">
+          <Notification title="Account must be an admin of the token" variant="primary" />
+        </div>
+        <div className="mint-token">
+          <Card variant="primary">
+            <Caption size="sm" addlClassName="step-count">
+              step {stepCount} of 8
+            </Caption>
+            {renderStep(stepCount)}
+          </Card>
+        </div>
+        {connectionError !== null &&
+          createPortal(
+            <div className="notification-container">
+              <Notification title={connectionError!} variant="error" />
+            </div>,
+            document.getElementById("root")!,
+          )}
+      </div>
+    </>
+  );
+};
